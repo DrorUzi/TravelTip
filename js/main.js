4e22a18f0c45aac9a347c373c2d4872779d44d73
Dror Uzi <dror.uzi5@gmail.com>
2019-10-30 15:29:26 +0200

    basic design

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ window.onload = () => {
         })
 }
 
-document.querySelector('.btn').addEventListener('click', (ev) => {
+document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
     console.log('Aha!', ev.target);
     mapService.panTo(35.6895, 139.6917);
 })
