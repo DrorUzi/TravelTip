a14bd93ebb7e239f1eeabfc644b8555644f815b5
Dror Uzi <dror.uzi5@gmail.com>
2019-10-31 09:37:22 +0200

    http

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -5,7 +5,7 @@ export default {
 }
 
 function getWeather(lat, lng) {
-    let prm = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=cdedac0cf3893bcce175970704b3af16`)
+    let prm = axios.get(`//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=cdedac0cf3893bcce175970704b3af16`)
     let currWeather = prm.then(res => {
         let data = res.data;
         let resWeather = Object.values(data)
