7691f39c21d53856591b784db333d3265d8d6e12
Dror Uzi <dror.uzi5@gmail.com>
2019-10-31 08:49:05 +0200

    z

--- js/main.js
+++ js/main.js
@@ -46,7 +46,6 @@ function renderGeocodeData(geocodeData) {
     mapService.setMapZoom(13)
 }
 
-// TODO design to the weather section  
 function renderWeather(lat, Lng) {
     weatherService.getWeather(lat, Lng)
         .then(res => {
